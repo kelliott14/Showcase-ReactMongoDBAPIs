bc675a3e9b5fc057f460e0b2eeda8182f7ad29d3
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-12-14 15:50:52 +1100

    deploy

---
+++ client/build/precache-manifest.885d799f618199fb756775cf86d94706.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f001b4a44c786b6617c",
+    "url": "/static/css/main.a433bc62.chunk.css"
+  },
+  {
+    "revision": "7f001b4a44c786b6617c",
+    "url": "/static/js/main.227de9f9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0951c2afd5b2c2676c05",
+    "url": "/static/js/2.06516cb7.chunk.js"
+  },
+  {
+    "revision": "07d247acd175b874b60ba0ed69a268f5",
+    "url": "/index.html"
+  }
+];
